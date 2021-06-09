00e21482963be4e264f9554e8a8477cfb1cb70ec
Alamin Howlader <alaminhowladerofficial@gmail.com>
2021-06-09 21:28:19 +0600

    updated

--- js/single-contact.js
+++ js/single-contact.js
@@ -1,34 +1,13 @@
+// collapsible
+$(document).ready(function () {
+    $('.collapsible').collapsible();
+});
+// modal
 $(document).ready(function () {
     $('.modal').modal();
 });
 
-
-// const addCC = document.getElementById('add__cc');
-
-// addCC.addEventListener('click', () => {
-//     document.getElementById('cc_select_field').classList.remove('d-none');
-//     addCC.style.visibility = 'hidden';
-// })
-
-// const closeCC = document.getElementById('close__cc');
-// closeCC.addEventListener('click', () => {
-//     document.getElementById('cc_select_field').classList.add('d-none');
-//     addCC.style.visibility = 'visible';
-// })
-// const addBCC = document.getElementById('add__bcc');
-
-// addBCC.addEventListener('click', () => {
-//     document.getElementById('bcc_select_field').classList.remove('d-none');
-//     addBCC.style.visibility = 'hidden';
-// })
-
-// const closeBCC = document.getElementById('close__bcc');
-// closeBCC.addEventListener('click', () => {
-//     document.getElementById('bcc_select_field').classList.add('d-none');
-//     addBCC.style.visibility = 'visible';
-// })
-
-
+// CC BCC handler
 const addCcBccHandler = (type) => {
     const handler = document.getElementById(`add__${type}`)
     const field = document.getElementById(`${type}_select_field`)
