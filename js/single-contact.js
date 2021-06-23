9af59cbf991717a9ac9536d255567323791ff22e
Alamin Howlader <alaminhowladerofficial@gmail.com>
2021-06-23 17:26:58 +0600

    updated

--- js/single-contact.js
+++ js/single-contact.js
@@ -23,3 +23,80 @@ const addCcBccHandler = (type) => {
 };
 addCcBccHandler('cc');
 addCcBccHandler('bcc');
+
+
+// Tiny Mce [Text]
+tinymce.init({
+    selector: 'textarea#basic-example-text',
+    placeholder: 'Type Here',
+    menubar: false,
+    branding: false,
+    height: 300,
+    plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen colorpicker',
+        'media paste imagetools help'
+    ],
+    toolbar: 'insertfile undo redo | styleselect | bold italic underline strikethrough | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image |print preview media | help',
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif;background:#fff; font-size:14px }'
+});
+// Tiny Mce [MMS]
+tinymce.init({
+    selector: 'textarea#basic-example-mms',
+    placeholder: 'Type Here',
+    menubar: false,
+    branding: false,
+    height: 300,
+    plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen colorpicker',
+        'media paste imagetools help'
+    ],
+    toolbar: 'insertfile undo redo | styleselect | bold italic underline strikethrough | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image |print preview media | help',
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif;background:#fff; font-size:14px }'
+});
+// Tiny Mce [Note]
+tinymce.init({
+    selector: 'textarea#basic-example-note',
+    placeholder: 'Type Here',
+    menubar: false,
+    branding: false,
+    height: 300,
+    plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen colorpicker',
+        'media paste imagetools help'
+    ],
+    toolbar: 'insertfile undo redo | styleselect | bold italic underline strikethrough | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image |print preview media | help',
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif;background:#fff; font-size:14px }'
+});
+// Tiny Mce [Email]
+tinymce.init({
+    selector: 'textarea#basic-example-email',
+    placeholder: 'Type Here',
+    menubar: false,
+    branding: false,
+    height: 200,
+    plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen colorpicker',
+        'media paste imagetools help'
+    ],
+    toolbar: 'insertfile undo redo | styleselect | bold italic underline strikethrough | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image |print preview media | help',
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif;background:#fff; font-size:14px }'
+});
+// Tiny Mce [Video Email]
+tinymce.init({
+    selector: 'textarea#basic-example-video-email',
+    placeholder: 'Type Here',
+    menubar: false,
+    branding: false,
+    height: 200,
+    plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen colorpicker',
+        'media paste imagetools help'
+    ],
+    toolbar: 'insertfile undo redo | styleselect | bold italic underline strikethrough | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image |print preview media | help',
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif;background:#fff; font-size:14px }'
+});
